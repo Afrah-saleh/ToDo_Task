7e53272ce387b9e301676bcc037a3047f056f860
Afrah-saleh <afrahsaleh49@hotmail.com>
2024-10-05 16:55:51 +0300

    update

--- App.js
+++ App.js
@@ -74,7 +74,6 @@ export default function App() {
               getToDoList={fetchToDoList} // Refresh list after updates
             />
           )}
-         // keyExtractor={(item) => item.id}
         />
       )}
 
